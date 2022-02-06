7612972e4685e4b03c05b39c33a0df68b862de14
gbryant-dev <gbryant1@hotmail.co.uk>
2022-02-06 21:18:28 +0000

    Set up styled-components

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import { StyledTitle } from "./index.style";
+
+
+export default function Home() {
+  return <StyledTitle>Hello, world!</StyledTitle>
+}
