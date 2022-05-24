225a683fbfbee0e5ad08c75cc175c1cdc00f6474
gbryant-dev <gbryant1@hotmail.co.uk>
2022-05-24 13:03:22 +0100

    fix guess distribution graph

--- components/GameStats/GuessDistribution/helper.ts
+++ components/GameStats/GuessDistribution/helper.ts
@@ -1,17 +1,17 @@
 import { GameStats } from "../../../types";
 
-// *TODO* fix width when fail is the most common 
 export const getWins = (guesses: GameStats['guesses']) => {
     // eslint-disable-next-line no-unused-vars
     const { fail, ...winGuesses } = guesses
-    const mostCommonKey = Object.keys(guesses).reduce((prev, curr) => guesses[curr] > guesses[prev] ? curr : prev)
+    const sorted = Object.entries(guesses).sort((a, b) => b[1] - a[1])
+    const [mostCommonKey, mostCommonValue] = sorted[0][0] === 'fail' ? sorted[1] : sorted[0]
     const data = {}
     for (const key in winGuesses) {
         const value = winGuesses[key]
-        if (key === mostCommonKey || value === winGuesses[mostCommonKey]) {
+        if ((key === mostCommonKey || value === winGuesses[mostCommonKey]) && mostCommonValue > 0) {
             data[key] = { value: value, proportion: 1 }
         } else {
-            data[key] = { value: value, proportion: value / winGuesses[mostCommonKey] }
+            data[key] = { value: value, proportion: value / mostCommonValue }
         }
     }
     return data
